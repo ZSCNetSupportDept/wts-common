1060a1cc3177baca7aab1542cb31e1584eacd236
Sola <dev@sola.love>
2018-03-25 06:55:44 +0800

    refactor
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/zscnsd/wts/common/oauth2/WtsUserAuthenticationConverter.kt
+++ src/main/kotlin/love/sola/zscnsd/wts/common/oauth2/WtsUserAuthenticationConverter.kt
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.provider.token.UserAuthenticationConverter
 
-class CustomUserAuthenticationConverter : UserAuthenticationConverter {
+class WtsUserAuthenticationConverter : UserAuthenticationConverter {
 
     private val objectMapper = jacksonObjectMapper().findAndRegisterModules()
 
