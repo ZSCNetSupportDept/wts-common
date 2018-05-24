f29e2be6fcd6913f9977235097593f56dd72abfb
Sola <dev@sola.love>
2018-05-24 11:12:53 +0800

    chunk instead of block
    Signed-off-by: Sola <dev@sola.love>

--- src/main/kotlin/love/sola/zscnsd/wts/common/domain/DutyArrangement.kt
+++ src/main/kotlin/love/sola/zscnsd/wts/common/domain/DutyArrangement.kt
@@ -3,4 +3,4 @@ package love.sola.zscnsd.wts.common.domain
 import java.time.DayOfWeek
 
 @ForJpaUse
-data class DutyArrangement(val week: DayOfWeek, val block: Block)
+data class DutyArrangement(val week: DayOfWeek, val chunk: Chunk)
